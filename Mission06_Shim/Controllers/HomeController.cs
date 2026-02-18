39695f54364a4f1342ea4d4d95a43d3b3e70dbcf
홍길동 <@naver.com>
2026-02-18 12:33:27 -0700

    test

--- Mission06_Shim/Controllers/HomeController.cs
+++ Mission06_Shim/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
 using Mission06_Shim.Models;
-using System.Diagnostics;
-using System.Linq; // Required for OrderBy
+using System.Linq;
 
 namespace Mission06_Shim.Controllers
 {
@@ -9,112 +9,88 @@ namespace Mission06_Shim.Controllers
     {
         private readonly MovieContext _context;
 
-        // Constructor: Injecting the database context
         public HomeController(MovieContext context)
         {
             _context = context;
         }
 
-        // GET: Home Page
-        public IActionResult Index()
+        public IActionResult Index() => View();
+        public IActionResult GetToKnow() => View();
+
+        
+        public IActionResult MovieList()
         {
-            return View();
+            var movies = _context.Movies
+                .Include(x => x.Category) 
+                .OrderBy(x => x.Title)
+                .ToList();
+            return View(movies);
         }
 
-        // GET: Get To Know Joel Page
-        public IActionResult GetToKnow()
-        {
-            return View();
-        }
-
-        // GET: Enter Movie Page (Create)
+        
         [HttpGet]
         public IActionResult EnterMovie()
         {
-            // Pass a new Movie object to the view to initialize the form
+           
+            ViewBag.Categories = _context.Categories.ToList();
             return View("EnterMovie", new Movie());
         }
 
-        // POST: Enter Movie Page (Create)
         [HttpPost]
         public IActionResult EnterMovie(Movie response)
         {
             if (ModelState.IsValid)
             {
-                // Add the new record to the database and save changes
                 _context.Movies.Add(response);
                 _context.SaveChanges();
-
-                // Show confirmation view
                 return View("Confirmation", response);
             }
             else
             {
-                // If validation fails, return the form with error messages
+               
+                ViewBag.Categories = _context.Categories.ToList();
                 return View(response);
             }
         }
 
-        // GET: Movie List Page (Read)
-        [HttpGet]
-        public IActionResult MovieList()
-        {
-            // Retrieve all movies from the database, sorted by Title
-            var movies = _context.Movies.OrderBy(x => x.Title).ToList();
-
-            return View(movies);
-        }
-
-        // GET: Edit Movie Page (Update)
+        
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            // Find the record that matches the ID passed in the URL
             var record = _context.Movies.Single(x => x.MovieId == id);
-
-            // Reuse the "EnterMovie" view, but pass the existing data
+            ViewBag.Categories = _context.Categories.ToList(); 
             return View("EnterMovie", record);
         }
 
-        // POST: Edit Movie Page (Update)
         [HttpPost]
         public IActionResult Edit(Movie updatedInfo)
         {
             if (ModelState.IsValid)
             {
-                // Update the existing record in the database
                 _context.Update(updatedInfo);
                 _context.SaveChanges();
-
-                // Redirect to the Movie List page after saving
                 return RedirectToAction("MovieList");
             }
             else
             {
-                // If validation fails, return the form with the data
+                ViewBag.Categories = _context.Categories.ToList();
                 return View("EnterMovie", updatedInfo);
             }
         }
 
-        // GET: Delete Confirmation Page (Delete)
+       
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            // Find the record to confirm deletion
             var record = _context.Movies.Single(x => x.MovieId == id);
-
             return View(record);
         }
 
-        // POST: Delete Action (Delete)
         [HttpPost]
         public IActionResult Delete(Movie movie)
         {
-            // Remove the record from the database
             _context.Movies.Remove(movie);
             _context.SaveChanges();
-
-            // Redirect to the Movie List page
             return RedirectToAction("MovieList");
         }
     }
--- Mission06_Shim/Models/Movie.cs
+++ Mission06_Shim/Models/Movie.cs
@@ -1,36 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Mission06_Shim.Models;
-
-public class Movie
+namespace Mission06_Shim.Models
 {
-    [Key]
-    [Required]
-    public int MovieId { get; set; }
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
 
-    [Required(ErrorMessage = "Category is required.")]
-    public string Category { get; set; }
+      
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; } 
 
-    [Required(ErrorMessage = "Title is required.")]
-    public string Title { get; set; }
+        [Required]
+        public string Title { get; set; }
 
-    [Required(ErrorMessage = "Year is required.")]
-    [Range(1888, int.MaxValue, ErrorMessage = "Year must be after 1888.")] // 1888년 제한
-    public int Year { get; set; }
+        [Required]
+        public int Year { get; set; }
 
-    [Required(ErrorMessage = "Director is required.")]
-    public string Director { get; set; }
-
-    [Required(ErrorMessage = "Rating is required.")]
-    public string Rating { get; set; }
-
-    public bool Edited { get; set; }
-
-    public string? LentTo { get; set; }
-
-    [MaxLength(25, ErrorMessage = "Notes cannot exceed 25 characters.")]
-    public string? Notes { get; set; }
-
-    
-    public bool CopiedToPlex { get; set; } 
+        public string? Director { get; set; }
+        public string? Rating { get; set; }
+        public bool Edited { get; set; }
+        public string? LentTo { get; set; }
+        public bool CopiedToPlex { get; set; }
+        public string? Notes { get; set; }
+    }
 }
